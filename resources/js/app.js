c2300cb986e2cc9c7c9e327a19a1099cf833d525
Boris <bobabonanadz@mail.ru>
2021-06-25 19:41:36 +0300

    Update Vuex 4. Create API Controller movie. Create api route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,7 @@
 import {createApp} from 'vue';
 import App from "./components/App";
+import store from "./store";
 
-createApp(App).mount("#app");
+createApp(App)
+    .use(store)
+    .mount("#app");
